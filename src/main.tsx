097f7202f4587717c4dc0bba9c04523811badc04
Sebaldric <sebaldric.seadog@outlook.jp>
2025-02-27 23:44:08 +0900

    feat: connetcted convex

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-createRoot(document.getElementById('root')!).render(
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConvexProvider client={convex}>
+      <div className="flex h-screen w-screen">
+        <App />
+      </div>
+    </ConvexProvider>
+  </StrictMode>
+);
